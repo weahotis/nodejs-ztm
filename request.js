eaa18f914443ceee642f660bd0b6d3610337568a
weahotis <weahotis9@gmail.com>
2023-07-20 14:39:41 +0000

    added

--- request.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-axios.get("https://wwwwww.google.com").then((response) => {
-  console.log(response);
-}).catch((err)=>{
-    console.log(err)
-}).then(()=>console.log('all done'));
