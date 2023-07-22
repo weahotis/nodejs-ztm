3a4ea5e5a982e50660133470c0f82a5287f7207b
weahotis <weahotis9@gmail.com>
2023-07-22 15:40:20 +0000

    created route parameter

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res)=>{
-    res.send('<ul><li>Hello Otis </li></ul>')
-})
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+const friends = [
+  {
+    id: 0,
+    name: "Jenkins Mulbah"
+  },
+  {
+    id: 1,
+    name: "Otis Weah"
+  }
+];
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId)
+  if(!friendId){
+    res.send(`<ul><li>firend with the id ${friendId} does not exist</li></ul>`)
+  }else {
+    res.send(friends.filter(f=>f.id === friendId))
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("<ul><li>Hello Otis </li></ul>");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
